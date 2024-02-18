896d0390d3ad8fda553a0c9a3c94fe5ac8cfa5e8
NadaMuhammed <nadamuhammed776@gmail.com>
2024-02-18 21:33:07 +0200

    Added Splash Screen.

---
+++ app/src/main/java/com/example/newsapp/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.newsapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        startHomeActivity()
+
+    }
+
+    private fun startHomeActivity() {
+        Handler(mainLooper).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }, 2000)
+    }
+}
