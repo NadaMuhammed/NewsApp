d3bad14e3c57b5a62f6fa5e723ff71e95af0cbcc
NadaMuhammed <nadamuhammed776@gmail.com>
2024-02-21 00:05:18 +0200

    Finished News Fragment Ui.

--- app/src/main/java/com/example/newsapp/ui/home/fragments/NewsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/home/fragments/NewsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.newsapp.ui.fragments
+package com.example.newsapp.ui.home.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,31 +6,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.newsapp.Constants
+import com.example.newsapp.api.model.Source
 import com.example.newsapp.api.model.SourcesResponse
 import com.example.newsapp.api.retrofit.ApiManager
-import com.example.newsapp.databinding.FragmentCategoryBinding
+import com.example.newsapp.databinding.FragmentNewsBinding
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class CategoryFragment : Fragment() {
-    lateinit var binding: FragmentCategoryBinding
+class NewsFragment : Fragment() {
+    lateinit var binding: FragmentNewsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        binding = FragmentNewsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loadSources()
+    }
+
+    private fun loadSources() {
         ApiManager.webServices.getSources(Constants.API_KEY, "sports").enqueue(object : Callback<SourcesResponse>{
             override fun onResponse(
                 call: Call<SourcesResponse>,
                 response: Response<SourcesResponse>
             ) {
-
+                if (response.isSuccessful){
+                    showTabs(response.body()?.sources)
+                } else{
+                    val errorResponse = Gson().fromJson(response.errorBody()?.string(), SourcesResponse::class.java)
+                    showErrorViews(errorResponse)
+                }
             }
 
             override fun onFailure(call: Call<SourcesResponse>, t: Throwable) {
@@ -38,5 +49,17 @@ class CategoryFragment : Fragment() {
             }
 
         })
+    }
+
+    private fun showErrorViews(sourcesResponse: SourcesResponse) {
+
+    }
+
+    private fun showTabs(sources: List<Source>?) {
+        sources?.forEach {source->
+            val tab = binding.tabLayout.newTab()
+            tab.text = source.name
+            binding.tabLayout.addTab(tab)
+        }
     }
 }
--- app/src/main/java/com/example/newsapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/splash/SplashActivity.kt
@@ -1,10 +1,11 @@
-package com.example.newsapp.ui.activities
+package com.example.newsapp.ui.splash
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.example.newsapp.R
+import com.example.newsapp.ui.home.HomeActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
